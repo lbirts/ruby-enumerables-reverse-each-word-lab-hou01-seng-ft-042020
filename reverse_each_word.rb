eba7683f2161691236921119d0545a986eb1f6f3
Lauren Birts <birtslauren@gmail.com>
2020-03-25 04:27:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   non_reverse = string.split(" ")
   reverse_array = []
-  reverse_array = non_reverse.each { |string| string.reverse }
+  reverse_array << non_reverse.each { |string| string.reverse }
   reverse_array.join(" ")
 end
